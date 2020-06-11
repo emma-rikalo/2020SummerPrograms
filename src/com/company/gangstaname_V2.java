1323fba386ee74134bb61b9fe2d33c93df2488db
emma-rikalo <rikaloe23@greenhill.org>
2020-06-11 10:01:12 -0500

    Merge remote-tracking branch 'origin/master'

--- src/com/company/gangstaname_V2.java
+++ src/com/company/gangstaname_V2.java
@@ -8,26 +8,31 @@ import java.util.Scanner;
 
 public class gangstaname_V2 {
 
-
     public static void main(String[] args) {
-       //create a string to represent the user's name
-       String name;
+        GangstaName();
+    }
 
-       //create a scanner to receive input
-       Scanner input= new Scanner (System.in);
-       System.out.print("Type your name: ");
+    //create a method
+    public static void GangstaName(){
+        //create a string to represent the user's name
+        String name;
 
-       //create Strings to represent the user's first and last names using indexOf
+        //create a scanner to receive input
+        Scanner input= new Scanner (System.in);
+        System.out.print("Type your name: ");
+
+        //create Strings to represent the user's first and last names using indexOf
         String fName= input.nextLine();
-       int indexOf= fName.indexOf(" ");
-       String LName= fName.substring(indexOf+1);
-       fName= fName.substring(0, indexOf);
+        int indexOf= fName.indexOf(" ");
+        String LName= fName.substring(indexOf+1);
+        fName= fName.substring(0, indexOf);
 
         //print out the gangsta name, using charAt to print the initial, and toUpperCase to capitalize all of the last name
-       System.out.print("Your gangsta name is: ");
-       System.out.print(fName.charAt(0) + ". " + "Diddy " +LName.toUpperCase() + " " + fName + "-izzle");
+        System.out.print("Your gangsta name is: ");
+        System.out.print(fName.charAt(0) + ". " + "Diddy " +LName.toUpperCase() + " " + fName + "-izzle");
 
     }
+
 
 }
 
